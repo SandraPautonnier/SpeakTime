e8d5ffb2a956df3ac198ec4795db5d6d87b0b69e
SandraPautonnier <sandrapautonnier@gmail.com>
2025-10-17 21:01:22 +0200

    fix: update footer year to 2025 and adjust padding; add margin to header banner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p>© 2024 SpeakTime. Tous droits réservés.</p>
+        <p>© 2025 SpeakTime. Tous droits réservés.</p>
     </footer>
   )
 }
